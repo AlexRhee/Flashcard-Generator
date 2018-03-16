f139f03c9f631acb191e28a12c9b5be90bd77874
arhee <arheeee@gmail.com>
2018-03-15 17:58:58 -0700

    v.2 fully working, 20 total questions

--- Main.js
+++ Main.js
@@ -2,108 +2,141 @@ var ClozeCard = require("./ClozeCard.js");
 var BasicCard = require("./BasicCard.js");
 var inquirer = require("inquirer");
 
+var basicArr = [];
+var clozeArr = [];
+
+var basicCount = 0;
+var clozeCount = 0;
+
 var winCount = 0;
 var loseCount = 0;
 
-var question1 = new ClozeCard("Kobe Bryant wore number 24 and number 8 for the Lakers", "Kobe Bryant");
-var question2 = new BasicCard("Which NBA Legend does the NBA Logo depict?", "Jerry West");
-var question3 = new ClozeCard("Doc Rivers is the coach of the Clippers", "Doc Rivers");
-var question4 = new BasicCard("How many championships did Kareem Abdul Jabbar win?", 6);
-var question5 = new ClozeCard("Wilt Chamberlain scored 100 points in a game, the most in NBA history.", "Wilt Chamberlain");
-var question6 = new BasicCard("What number did Tim Duncan wear in the NBA?", 21);
-var question7 = new ClozeCard("Kevin Durant goes by the nickname 'cupcake' and 'snake'.", "Kevin Durant");
-var question8 = new BasicCard("How many points did Tracy McGrady famously score in 31 seconds to beat the Spurs", 13);
-var question9 = new ClozeCard("Greg Oden was drafted one pick before Kevin Durant in the 2007 NBA Draft.", "Greg Oden");
-var question10 = new BasicCard("Which team blew a 3-1 lead in the NBA Finals? - team name only, no city, no the", "Warriors");
 
-function game() {
+//cloze card questions
+clozeArr.push(new ClozeCard("Kobe Bryant wore number 24 and number 8 for the Lakers.", "Kobe Bryant"));
+clozeArr.push(new ClozeCard("Doc Rivers is the coach of the Clippers.", "Doc Rivers"));
+clozeArr.push(new ClozeCard("Wilt Chamberlain scored 100 points in a game, the most in NBA history.", "Wilt Chamberlain"));
+clozeArr.push(new ClozeCard("Kevin Durant goes by the nickname 'cupcake' and 'snake'.", "Kevin Durant"));
+clozeArr.push(new ClozeCard("Greg Oden was drafted one pick before Kevin Durant in the 2007 NBA Draft.", "Greg Oden"))
+clozeArr.push(new ClozeCard("Vince Carter goes by the nickname half man half amazing.", "Vince Carter"));
+clozeArr.push(new ClozeCard("Marcus Morris is the twin brother of NBA player Markieff Morris.", "Marcus Morris"));
+clozeArr.push(new ClozeCard("Blake Griffin won the dunk contest by jumping over a Kia.", "Blake Griffin"));
+clozeArr.push(new ClozeCard("Steve Francis was the runner up to Vince Carter in the 2000 dunk contest.", "Steve Francis"));
+clozeArr.push(new ClozeCard("Russell Westbrook was the last player to average a triple double for the season", "Russell Westbrook"));
+
+//basic card questions
+basicArr.push(new BasicCard("Which NBA Legend does the NBA Logo depict?", "Jerry West"));
+basicArr.push(new BasicCard("Which team blew a 3-1 lead in the NBA Finals? - team name only, no city, no the: ", "Warriors"));
+basicArr.push(new BasicCard("How many points did Tracy McGrady famously score in 31 seconds to beat the Spurs?", "13"));
+basicArr.push(new BasicCard("How many championships did Kareem Abdul Jabbar win?", "6"));
+basicArr.push(new BasicCard("What number did Tim Duncan wear in the NBA?", "21"));
+basicArr.push(new BasicCard("What country is Spurs player Manu Ginobili from?", "Argentina"));
+basicArr.push(new BasicCard("Who won the 2017 sixth man of the year award and also the 2017 three point contest?", "Eric Gordon"));
+basicArr.push(new BasicCard("Which player holds the record for most threes made in a season?", "Stephen Curry"));
+basicArr.push(new BasicCard("Who goes by the nickname 'The Process'?", "Joel Embiid"));
+basicArr.push(new BasicCard("Which NBA bust was drafted right after Lebron James in the 2003 NBA Draft?", "Darko Milicic"));
+
+
+function quiz() {
+
+basicCount = 0;
+clozeCount = 0;
+
+winCount = 0;
+loseCount = 0;
 
     inquirer.prompt([{
-        type: "input",
-        name: "q1",
-        message: "_________" + question1.partial
-    }]).then(function (data) {
-        if (data.q1 === question1.cloze) {
-            console.log("correct")
-            winCount++;
-        }
-        else {
-            console.log("no");
-            loseCount++;
-        }
-
-        inquirer.prompt([{
-            type: "input",
-            name: "q2",
-            message: question2.front
-        }]).then(function (data) {
-            if (data.q2 === question2.back) {
-                console.log("correct")
-                winCount++;
-                console.log(winCount);
-            }
-            else {
-                console.log("no");
-                loseCount++;
-                console.log(loseCount);
-            }
-            inquirer.prompt([{
-                type: "input",
-                name: "q3",
-                message: "_________" + question3.partial
-            }]).then(function (data) {
-                if (data.q3 === question3.cloze) {
-                    console.log("correct")
-                    winCount++;
-                    console.log(winCount);
-                }
-                else {
-                    console.log("no");
-                    loseCount++;
-                    console.log(loseCount);
-                }
-                inquirer.prompt([{
-                    type: "input",
-                    name: "q4",
-                    message: question4.front
-                }]).then(function (data) {
-                    if (data.q4 === question4.back) {
-                        console.log("correct")
-                        winCount++;
-                        console.log(winCount);
-                    }
-                    else {
-                        console.log("no");
-                        loseCount++;
-                        console.log(loseCount);
-                    }
+        type: "list",
+        name: "type",
+        message: "What kind of flashcard quiz would you like to play?",
+        choices: ["Basic Card", "Cloze Card"]
+    }]).then(function (card) {
+        if (card.type === "Basic Card") {
+            function basicQuestion() {
+                if (basicCount < basicArr.length) {
                     inquirer.prompt([{
                         type: "input",
-                        name: "q5",
-                        message: "_________" + question4.partial
+                        name: "q",
+                        message: basicArr[basicCount].front
                     }]).then(function (data) {
-                        if (data.q5 === question5.cloze) {
-                            console.log("correct")
+                        if (data.q === basicArr[basicCount].back) {
+                            console.log("Answer: " + basicArr[basicCount].back)
                             winCount++;
-                            console.log(winCount);
+                            console.log("Correct: " + winCount);
+                            console.log("Incorrect: " + loseCount);
                         }
                         else {
-                            console.log("no");
+                            console.log("Answer: " + basicArr[basicCount].back)
                             loseCount++;
-                            console.log(loseCount);
+                            console.log("Correct: " + winCount);
+                            console.log("Incorrect: " + loseCount);
                         }
-    
+                        basicCount++;
+                        basicQuestion();
                     })
-                })
-
-            })
-        })
-    });
-
-
-
-}
-
-game();
-
-//console.log(holder)
+                }
+                else {
+                    console.log("Game Over")
+                    inquirer.prompt([{
+                        type: "list",
+                        name: "again",
+                        message: "Play again?",
+                        choices: ["Yes", "No"]
+                    }]).then(function(res){
+                        if (res.again === "Yes"){
+                            quiz();
+                        }
+                        else {
+                            process.end;
+                        }
+                    })
+                }
+            }
+            basicQuestion();
+        }
+        else {
+            function clozeQuestion() {
+                if (clozeCount < clozeArr.length) {
+                    inquirer.prompt([{
+                        type: "input",
+                        name: "q",
+                        message: "____________" + clozeArr[clozeCount].partial
+                    }]).then(function (data) {
+                        if (data.q === clozeArr[clozeCount].cloze) {
+                            console.log("Answer: " + clozeArr[clozeCount].fullText);
+                            winCount++;
+                            console.log("Correct: " + winCount);
+                            console.log("Incorrect: " + loseCount);
+                        }
+                        else {
+                            console.log("Answer: " + clozeArr[clozeCount].fullText);
+                            loseCount++;
+                            console.log("Correct: " + winCount);
+                            console.log("Incorrect: " + loseCount);
+                        }
+                        clozeCount++;
+                        clozeQuestion();
+                    })
+                }
+                else {
+                    console.log("Game Over")
+                    inquirer.prompt([{
+                        type: "list",
+                        name: "again",
+                        message: "Play again?",
+                        choices: ["Yes", "No"]
+                    }]).then(function(res){
+                        if (res.again === "Yes"){
+                            quiz();
+                        }
+                        else {
+                            process.end;
+                        }
+                    })
+                }
+            }
+            clozeQuestion();
+        }
+    })
+};
+quiz();
